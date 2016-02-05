57e673e635681d4c052e0e77dd53e09a8bfef048
NAMHYUNSUK <intern19@lotte.net>
2016-02-05 13:57:53 +0900

    VO객체 정의

--- LotteMall/src/com/vo/AccountVO.java
+++ LotteMall/src/com/vo/AccountVO.java
@@ -1,5 +1,48 @@
 package com.vo;
 
-public class AccountVO {
-
+public class AccountVO extends PagingVO{
+	private int idx;
+	private String id;
+	private String name;
+	private String password;
+	private int point;
+	private int phone_num;
+	
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public int getPhone_num() {
+		return phone_num;
+	}
+	public void setPhone_num(int phone_num) {
+		this.phone_num = phone_num;
+	}
 }
--- LotteMall/src/com/vo/MarketVO.java
+++ LotteMall/src/com/vo/MarketVO.java
@@ -1,5 +1,42 @@
 package com.vo;
 
-public class MarketVO {
-
+public class MarketVO extends PagingVO{
+	private int idx;
+	private String name;
+	private int phone_num;
+	private String latitude;
+	private String longitude;
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhone_num() {
+		return phone_num;
+	}
+	public void setPhone_num(int phone_num) {
+		this.phone_num = phone_num;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	
+	
+	
 }
